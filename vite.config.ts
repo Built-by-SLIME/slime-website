ba4038aeb4f7bbfae32631b07917a4a69eb1bb19
HeftySammich <support@heyproxy.io>
2026-01-22 14:13:57 -0500

    Configure Vite to properly handle Hedera dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@hashgraph/hedera-wallet-connect',
+      '@hashgraph/proto',
+      '@hashgraph/sdk',
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true,
+    },
+  },
 })
